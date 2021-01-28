c6c22fd187cc5291f142e18a5db363e360d1c232
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2021-01-28 17:29:14 +0200

    added files and notes page for the staff member

--- src/components/settings/staffmembers/components/userview/components/filesandnotes/index.js
+++ src/components/settings/staffmembers/components/userview/components/filesandnotes/index.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
 
 import Styles from "./filesandnotes.module.scss";
 
 const FilesAndNotes = (props) => {
-  return <div className={Styles.files}>Files and FilesAndNotes</div>;
+  return (
+    <div className={Styles.files}>
+      <h3 className={Styles.files__heading}>Staff member files</h3>
+      <div className={Styles.files__notes}>
+        <h5 className={Styles.files__notes__heading}>Notes</h5>
+        <div className={Styles.files__notes__notes}>
+          <Field
+            name='note'
+            component='textarea'
+            className={Styles.files__notes__notes__textarea}
+          />
+          <button className={Styles.files__notes__notes__button}>Add</button>
+        </div>
+      </div>
+    </div>
+  );
 };
-export default FilesAndNotes;
+export default reduxForm({ form: "addStaffMemberNotes" })(FilesAndNotes);
