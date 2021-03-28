e1dd60de5763e3f6f9c771e5bff089cdee14e1c6
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2021-03-28 14:49:14 +0200

    added files and notes & costitems pages for the add quote page

--- src/components/quotes/add-quote/components/cost-items/index.js
+++ src/components/quotes/add-quote/components/cost-items/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import Styles from "./cost-items.module.scss";
 
 const CostItems = (props) => {
-  return <div className={Styles.costitems}>Cost Items Goes Here....</div>;
+  return (
+    <div className={Styles.costitems}>
+      <p>Please save the quote prior to adding Cost items.</p>
+    </div>
+  );
 };
 
 export default CostItems;
--- src/components/quotes/add-quote/components/files-and-notes/index.js
+++ src/components/quotes/add-quote/components/files-and-notes/index.js
@@ -3,7 +3,9 @@ import Styles from "./files-and-notes.module.scss";
 
 const FilesAndNotes = (props) => {
   return (
-    <div className={Styles.filesandnotes}>Files And Notes Goes Here....</div>
+    <div className={Styles.filesandnotes}>
+      <p>Please save the quote prior to adding notes/documents.</p>
+    </div>
   );
 };
 
